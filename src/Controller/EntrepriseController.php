042a89bc7017544355bdcfac95c1bb1b9997335c
Aminebncd <amine.bounachada@hotmail.com>
2024-02-29 22:46:02 +0100

    travail sur les controllers et vues

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Repository\EntrepriseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,11 +12,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(EntityManagerInterface $entityManager): Response
+    // public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
         // $name = 'Elan Formation';
         // $tableau = ['1', '2', '3'];
-        $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+
+        // $entreprises = $entityManager->getRepository(Entreprise::class)->findAll();
+        // $entreprises = $entrepriseRepository->findAll();
+        // -> SELECT * FROM entreprise ORDER BY raisonSociale ASC
+        $entreprises = $entrepriseRepository->findBy(['ville' => 'strasbourg'], ['raisonSociale' => 'ASC']);
         return $this->render('entreprise/index.html.twig', [
             // 'name' => $name,
             // 'tableau' => $tableau,
